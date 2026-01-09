c4c5c53ebba88980516118503d5a7dbe19726008
giovani <giovani.avila.dev@gmail.com>
2026-01-09 16:04:20 -0300

    feat: update site and base configuration in astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://giovanicavila.github.io',
+  base: '/DOM-enlightenment',
   output: "static",
   prefetch: true,
   compressHTML: true,
